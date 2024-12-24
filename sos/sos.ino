52586c43bf18e8eb97036e3a5dae2e41fff2b32e
SpxceMan <anshul.sekuri@gmail.com>
2024-12-24 16:00:51 +0530

    SOS code has been added

---
+++ sos/sos.ino
@@ -0,0 +1,44 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(2000); 
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(2000); 
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(2000); 
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(300);
+}
